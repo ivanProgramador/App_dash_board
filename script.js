2fee37058bd72a1262533897737cebfe5763578e
ivan <ivan-guita@hotmail.com>
2022-02-20 21:07:48 -0300

    função $.get() para requisição de dados compreendida e implementada

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 $(document).ready(() => {
-	
+  
+  /*Toda a logica contida no http resquest esta encapsulada nessa função load
+    nesse caso quando o cliente clicar ela vai puxar o arquivo documentacao.html
+    que esta no servidor e substituit todo o conteudo que esta na div #pagina
+    por ele.
+  */
+
+  $('#documentacao').on('click',() => {
+
+  	// $('#pagina').load('documentacao.html')
+  	// uma segunda opção é usar a funçao $.get() que permite 
+  	//pegar os dados e depois decidir oque fazer com eles
+  	//no primeiro parametro ele recebe a url e no segundo uma função
+  	//que captura o conteudo de texto contido no arquivo referenciado
+
+  	$.get('documentacao.html', data => {
+  		
+  		$('#pagina').html(data)
+
+  	    }) 
+     })
+
+
+   $('#suporte').on('click',() => {
+
+  	$.get('suporte.html', data => {
+  		
+  	  $('#pagina').html(data)
+
+  	}) 
+
+
+  })
+
 })
